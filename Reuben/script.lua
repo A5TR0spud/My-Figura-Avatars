f990a67c68d1bd7705e4c1f7e7b3d7bddd4d0294
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2025-03-04 20:34:22 -0500

    gadgeknife

--- Reuben/script.lua
+++ Reuben/script.lua
@@ -1,15 +1,23 @@
--- Auto generated script file --
-
---hide vanilla model
 vanilla_model.PLAYER:setVisible(false)
-
---hide vanilla armor model
 vanilla_model.ARMOR:setVisible(false)
---re-enable the helmet item
 vanilla_model.HELMET_ITEM:setVisible(true)
-
---hide vanilla cape model
 vanilla_model.CAPE:setVisible(false)
-
---hide vanilla elytra model
 vanilla_model.ELYTRA:setVisible(false)
+
+local mainPage = action_wheel:newPage()
+action_wheel:setPage(mainPage)
+
+function pings.techno(b)
+    models.model.root:setUV(0, b and 0.5 or 0)
+    models.model.Skull:setUV(0, b and 0.5 or 0)
+end
+
+local action_Techno = mainPage:newAction()
+  :title("Pig")
+  :item("minecraft:pig_spawn_egg")
+  :setToggleItem("minecraft:red_carpet")
+  :setToggleTitle("Mechasword")
+  :onToggle(pings.techno)
+  :hoverColor(0.8, 0.8, 0.8)
+  :setToggleColor(0,0,0)
+--
