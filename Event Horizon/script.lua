ae5864d310b430b522ca97af3f092ca323541942
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2025-03-12 20:11:03 -0400

    Event Horizon Emission Preview + Skull

--- Event Horizon/script.lua
+++ Event Horizon/script.lua
@@ -6,10 +6,15 @@ vanilla_model.ELYTRA:setVisible(false)
 
 function events.entity_init()
   models.model.root:setPrimaryRenderType("CUTOUT_CULL")
-  --models.model.root:setSecondaryRenderType("CUTOUT_EMISSIVE_SOLID")
+  models.model.root:setSecondaryRenderType("EYES")
 end
 
+
+models.model.Skull:setPrimaryRenderType("CUTOUT_CULL")
+models.model.Skull:setSecondaryRenderType("EYES")
+
 local v = 0
+local u = 0
 local s = 0.5
 
 function events.tick()
@@ -17,5 +22,6 @@ function events.tick()
 end
 
 function events.render(delta, context)
-  models.model.root.Head.Ring:setRot(0, v + s * delta, 0)
+  local r = vectors:vec3(0, v + s * delta, 0)
+  models.model.root.Head.Ring:setRot(r)
 end
