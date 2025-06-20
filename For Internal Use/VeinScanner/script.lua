360eb1e6f266483b45bc9e8a61f96a138fe1949c
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2025-06-19 21:42:06 -0400

    Veinscanner Optimize + Fair Play + Counter

--- For Internal Use/VeinScanner/script.lua
+++ For Internal Use/VeinScanner/script.lua
@@ -20,6 +20,8 @@ textures:fromVanilla("quartz", "textures/item/quartz.png")
 local trackedBlocks = {}
 local seedBlock = nil
 
+local seedNum = models.model.HUD:newText("veinscanner_t"):setVisible(false):setBackground(true)
+
 local map = {
   {"minecraft:coal_ore", "coal"},
   {"minecraft:deepslate_coal_ore", "coal"},
@@ -51,15 +53,16 @@ local map = {
 }
 
 local function tryAddBlock(pos, dist)
-  if dist > 16 and #trackedBlocks > 24 then do return nil end end
-  if dist > 48 or #trackedBlocks > 496 then do return nil end end
   local id = world.getBlockState(pos):getID()
+  if seedBlock ~= nil and id ~= seedBlock[1] then do return nil end end
   for index, value in ipairs(trackedBlocks) do
     if value[2].x == pos.x and value[2].y == pos.y and value[2].z == pos.z then
       do return value end
     end
   end
-  local blockStore = {id, pos, math.floor(-dist / 6), nil, nil, dist}
+  if dist > 16 and #trackedBlocks > 24 then do return nil end end
+  if dist > 48 or #trackedBlocks > 64 then do return nil end end
+  local blockStore = {id, pos, 0, nil, pos, dist}
   if seedBlock ~= nil then
     blockStore[5] = seedBlock[2]
   end
@@ -68,7 +71,7 @@ local function tryAddBlock(pos, dist)
     if id == value[1] then
       blockValid = true
       local tex = textures:get(value[2])
-      blockStore[4] = models.model.HUD:newSprite("s_".. pos.x.. "_".. pos.y.. "_".. pos.z)
+      blockStore[4] = models.model.HUD:newSprite("veinscanner_s_".. #trackedBlocks)
         :setTexture(tex, tex:getDimensions().x, tex:getDimensions().y)
         :setSize(16, 16)
         --:setRenderType("TRANSLUCENT")
@@ -84,17 +87,35 @@ end
 
 events.TICK:register(function ()
   local block = player:getTargetedBlock(true)
-  --print(block)
-  --print(#trackedBlocks)
-  --print(trackedBlocks[1])
-  -- blockLocation = block[2]
-  seedBlock = tryAddBlock(block:getPos(), 0)
+  local newPos = block:getPos()
 
-  for _index, value in ipairs(trackedBlocks) do
-    if seedBlock == nil or seedBlock[2] ~= value[5] or world.getBlockState(seedBlock[2]):getID() ~= seedBlock[1] or value[1] ~= world.getBlockState(value[2]):getID() then
-      value[4]:remove()
+  local samePos = seedBlock == nil or seedBlock[2] == newPos
+
+  seedBlock = tryAddBlock(newPos, 0)
+
+  local recalc = true
+  if not samePos then
+    for index, value in ipairs(trackedBlocks) do
+      if value[2] == newPos then
+        recalc = false
+        break;
+      end
+    end
+    if not recalc then
+      for index, value in ipairs(trackedBlocks) do
+      value[5] = newPos
+    end
+    end
+  end
+
+  local _index = 1
+  while recalc and _index <= #trackedBlocks do
+    local value = trackedBlocks[_index]
+    if value ~= nil and (seedBlock == nil or seedBlock[2] ~= value[5] or world.getBlockState(value[2]):getID() ~= value[1]) then
       table.remove(trackedBlocks, _index)
-    elseif seedBlock ~= nil then
+      value[4]:setVisible(false)
+      _index = _index - 1
+    else
       value[3] = value[3] + 1
       local dist = value[6]
       if value[3] == 1 then
@@ -130,7 +151,14 @@ events.TICK:register(function ()
         tryAddBlock(value[2] + vectors:vec3(1, 1, 1), dist + 5)
       end
     end
+    _index = _index + 1
   end
+  local trackedNumText = "".. #trackedBlocks
+  if #trackedBlocks == 65 then
+    trackedNumText = "64+"
+  end
+  seedNum:setText(trackedNumText)
+  seedNum:setVisible(seedBlock ~= nil)
 end)
 
 events.POST_WORLD_RENDER:register(function (delta)
@@ -142,6 +170,16 @@ events.POST_WORLD_RENDER:register(function (delta)
     value[4]:setPos(p.x + s * 8, p.y + s * 8, p.w)
     value[4]:setVisible(p.z > 1)
     value[4]:setScale(s)
+  end
+  
+  if seedBlock ~= nil then
+    local p = vectors.worldToScreenSpace(seedBlock[2] + vectors:vec3(0.5, 0.5, 0.5))
+    p.x = (p.x * 0.5 + 0.5) * client.getWindowSize().x * -0.5
+    p.y = (p.y * 0.5 + 0.5) * client.getWindowSize().y * -0.5
+    local s = 5 * p.z / p.w
+    seedNum:setPos(p.x + s * 8 - s * 16, p.y + s * 8 - s * 16, p.w)
+    seedNum:setVisible(p.z > 1)
+    seedNum:setScale(s)
   end
 end)
 
